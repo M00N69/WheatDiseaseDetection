e2d5756956b1bc1736cf349723258b0db1b4eb8a
Harchet13 <harchet.majithia@gmail.com>
2024-06-22 14:46:20 +0530

    Add files via upload

---
+++ streamlit_app.py
@@ -0,0 +1,48 @@
+
+import streamlit as st
+
+with st.container():
+	col = st.columns([0.2,0.8])
+	col[0].image('logo.png')
+	col[1].title('Wheat Disease Detection')
+	
+with st.container(border=True):
+        st.header('About the App')
+        st.write('''This Wheat Disease Detection App identifies the disease in a wheat crop and suggests a cure.
+        Wheat is attacked by many different diseases caused by various pathogens and pests.
+        Around 21.5% of wheat production is lost to these diseases annually, totalling losses of 209 million tonnes worth $31 billion.''')
+        col = st.columns(5)
+        with col[0]:
+                st.image('black_rust_1.jpeg')
+                st.write('Black Rust')
+        with col[1]:
+                st.image('blast_1.jpeg')
+                st.write('Blast')
+        with col[2]:
+                st.image('smut_test_0.png')
+                st.write('Smut')
+        with col[3]:
+                st.image('fusarium_head_blight_test_0.png')
+                st.write('Fusarium Head Blight')
+        with col[4]:
+                st.image('tan_spot_24.jpeg')
+                st.write('Tan Spot')
+                
+        st.write('''It imports ultralytics and uses the YOLOv8 model to train
+and test data. YOLOv8, an evolution in the YOLO (You Only Look Once)
+series of real time detection model. Its core functionality revolves
+around a single-stage detector that processes images in one pass through
+the network, making it highly efficient for real-time applications.''')
+        st.subheader('Steps to use the app')
+        st.markdown('''
+        - Take a clear image
+        - Upload the image
+        - Analyse the image and the name of the disease will be written in the result panel below''')
+
+
+with st.container():
+        st.file_uploader('Upload your image',type=['jpg','png','jpeg'])
+        st.button('Analyse')
+
+
+      
