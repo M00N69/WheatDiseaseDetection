7bc8bde6c5f86a69361e7b56f7006f44b9dc483e
vestrol <abhinav.rathi@athenaeducation.co>
2024-08-10 13:01:59 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,12 +17,6 @@ with streamlit_analytics.track():
     	}
     	</style>
     	''',unsafe_allow_html=True)
-	st.title("NBA Analysis")
-	st.image("palm-tree-sunset-basketball-hoop-hwnjg9y6ee0i1ppj.jpg")
-	st.markdown("""
-	This site was created using Python, Streamlit and HTML in order to create pages showing how we can classify past NBA players data. On top of this, we have created a fantasy game which the user can interact with.  
-	
-	""")
 
 	st.set_page_config(layout="wide")
 	
