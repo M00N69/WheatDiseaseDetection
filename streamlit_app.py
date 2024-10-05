f9231c9b965e4d9dcc8505afbc1167318d46128a
vestrol <abhinav.rathi@athenaeducation.co>
2024-10-05 16:58:53 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -857,9 +857,9 @@ else:
 	    	''',unsafe_allow_html=True)
 		st.markdown('''<style>
 		[data-testid="column"] {
-		    width: calc(33.3333% - 1rem) !important;
-		    flex: 1 1 calc(33.3333% - 1rem) !important;
-		    min-width: calc(33% - 1rem) !important;
+		    width: calc(25% - 1rem) !important;
+		    flex: 1 1 calc(25% - 1rem) !important;
+		    min-width: calc(20% - 1rem) !important;
 		}
 		</style>''', unsafe_allow_html=True)
 		
